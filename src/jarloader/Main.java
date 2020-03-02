718e810ae9c792ed1b68d98b559a3c0bd43e2694
dandaman2 <dfduff@wpi.edu>
2020-03-02 18:32:25 -0500

    Added Windows dll paths to JAR execution on versions above 8

--- src/jarloader/Main.java
+++ src/jarloader/Main.java
@@ -1,25 +1,7 @@
 package jarloader;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
-public class Main extends Application {
-
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("loader.fxml"));
-        primaryStage.setTitle("Combinators.org Solitaire Jar Downloader");
-        primaryStage.setScene(new Scene(root, 800, 400));
-        primaryStage.setResizable(false);
-        primaryStage.show();
-
-    }
-
-
+public class Main {
     public static void main(String[] args) {
-        launch(args);
+        Launcher.main(args);
     }
 }
